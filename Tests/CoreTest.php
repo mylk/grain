67d7e698c938cc3c83e2f5093272a9978b8afae5
Kostas Milonas <milonas.ko@gmail.com>
2016-04-29 17:15:43 +0300

    replaces usage of static phpunit methods in test suites

--- Tests/CoreTest.php
+++ Tests/CoreTest.php
@@ -36,13 +36,16 @@ class CoreTest extends \PHPUnit_Framework_TestCase
         
         $core->map("/", "MyProject:User:edit");
         
-        $this->assertEquals(array(
-            "/" => array(
-                "controller" => "MyProject:User:edit",
-                "parameters" => array(),
-                "parameterPositions" =>  array()
-            )
-        ), \PHPUnit_Framework_Assert::readAttribute($core, "routes"));
+        $this->assertEquals(
+            array(
+                "/" => array(
+                    "controller" => "MyProject:User:edit",
+                    "parameters" => array(),
+                    "parameterPositions" =>  array()
+                )
+            ),
+            $this->readAttribute($core, "routes")
+        );
     }
     
     public function testMapRouteWithParameterOneAndOnly()
@@ -52,13 +55,16 @@ class CoreTest extends \PHPUnit_Framework_TestCase
         
         $core->map("/{id}", "MyProject:User:edit");
         
-        $this->assertEquals(array(
-            "/{id}" => array(
-                "controller" => "MyProject:User:edit",
-                "parameters" => array("id"),
-                "parameterPositions" =>  array(1)
-            )
-        ), \PHPUnit_Framework_Assert::readAttribute($core, "routes"));
+        $this->assertEquals(
+            array(
+                "/{id}" => array(
+                    "controller" => "MyProject:User:edit",
+                    "parameters" => array("id"),
+                    "parameterPositions" =>  array(1)
+                )
+            ),
+            $this->readAttribute($core, "routes")
+        );
     }
     
     public function testMapRouteWithParameterBeginning()
@@ -68,13 +74,16 @@ class CoreTest extends \PHPUnit_Framework_TestCase
         
         $core->map("/{id}/edit", "MyProject:User:edit");
         
-        $this->assertEquals(array(
-            "/{id}/edit" => array(
-                "controller" => "MyProject:User:edit",
-                "parameters" => array("id"),
-                "parameterPositions" =>  array(1)
-            )
-        ), \PHPUnit_Framework_Assert::readAttribute($core, "routes"));
+        $this->assertEquals(
+            array(
+                "/{id}/edit" => array(
+                    "controller" => "MyProject:User:edit",
+                    "parameters" => array("id"),
+                    "parameterPositions" =>  array(1)
+                )
+            ),
+            $this->readAttribute($core, "routes")
+        );
     }
     
     public function testMapRouteWithParameterMiddle()
@@ -84,13 +93,16 @@ class CoreTest extends \PHPUnit_Framework_TestCase
         
         $core->map("/user/{id}/edit", "MyProject:User:edit");
         
-        $this->assertEquals(array(
-            "/user/{id}/edit" => array(
-                "controller" => "MyProject:User:edit",
-                "parameters" => array("id"),
-                "parameterPositions" =>  array(2)
-            )
-        ), \PHPUnit_Framework_Assert::readAttribute($core, "routes"));
+        $this->assertEquals(
+            array(
+                "/user/{id}/edit" => array(
+                    "controller" => "MyProject:User:edit",
+                    "parameters" => array("id"),
+                    "parameterPositions" =>  array(2)
+                )
+            ),
+            $this->readAttribute($core, "routes")
+        );
     }
     
     public function testMapRouteWithParameterEnd()
@@ -100,13 +112,16 @@ class CoreTest extends \PHPUnit_Framework_TestCase
         
         $core->map("/user/edit/{id}", "MyProject:User:edit");
         
-        $this->assertEquals(array(
-            "/user/edit/{id}" => array(
-                "controller" => "MyProject:User:edit",
-                "parameters" => array("id"),
-                "parameterPositions" =>  array(3)
-            )
-        ), \PHPUnit_Framework_Assert::readAttribute($core, "routes"));
+        $this->assertEquals(
+            array(
+                "/user/edit/{id}" => array(
+                    "controller" => "MyProject:User:edit",
+                    "parameters" => array("id"),
+                    "parameterPositions" =>  array(3)
+                )
+            ),
+            $this->readAttribute($core, "routes")
+        );
     }
     
     public function testMapRouteWithTwoParameters()
@@ -116,13 +131,16 @@ class CoreTest extends \PHPUnit_Framework_TestCase
         
         $core->map("/user/edit/{id}/{version}", "MyProject:User:edit");
         
-        $this->assertEquals(array(
-            "/user/edit/{id}/{version}" => array(
-                "controller" => "MyProject:User:edit",
-                "parameters" => array("id", "version"),
-                "parameterPositions" =>  array(3, 4)
-            )
-        ), \PHPUnit_Framework_Assert::readAttribute($core, "routes"));
+        $this->assertEquals(
+            array(
+                "/user/edit/{id}/{version}" => array(
+                    "controller" => "MyProject:User:edit",
+                    "parameters" => array("id", "version"),
+                    "parameterPositions" =>  array(3, 4)
+                )
+            ),
+            $this->readAttribute($core, "routes")
+        );
     }
     
     /**
