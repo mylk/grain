048c9e2177742a8792fd11b0e363f5ba90aff5d6
Kostas Milonas <milonas.ko@gmail.com>
2016-05-08 22:04:31 +0300

    adds definitions of missing tests

---
+++ Tests/ControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Grain\Tests;
+
+class ControllerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetDbConfigurationDoesNotExist()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGetDbConfigurationExists()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGenerateUrlForNonExistingRoute()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testGenerateUrlForExistingRoute()
+    {
+        $this->markTestIncomplete();
+    }
+}
---
+++ Tests/DatabaseTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Grain\Tests;
+
+use Grain\Database;
+
+class DatabaseTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConnectToDatabaseServer()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testDisconnectFromDatabaseServer()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testExecuteInvalidQuery()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testExecuteNotExistingTable()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testExecuteNoResult()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testExecuteSingleResult()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testExecuteMultipleResults()
+    {
+        $this->markTestIncomplete();
+    }
+}
