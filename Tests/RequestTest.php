0359765225e372f45eeb695762bb3752623091fc
Kostas Milonas <milonas.ko@gmail.com>
2016-05-08 14:52:05 +0300

    tests parsing request data for get requests with parameters, post requests, post requests with json content type

--- Tests/RequestTest.php
+++ Tests/RequestTest.php
@@ -13,8 +13,8 @@ class RequestTests extends \PHPUnit_Framework_TestCase
             "controller" => "MyProject:MyController",
             "parameters" => array(),
             "parameterPositions" => array()
-        ), "");
-        
+        ), "text/plain");
+
         $this->assertEquals(array(), $parameters);
     }
 
@@ -25,11 +25,11 @@ class RequestTests extends \PHPUnit_Framework_TestCase
             "controller" => "MyProject:MyController",
             "parameters" => array("id"),
             "parameterPositions" => array(1)
-        ), "");
-        
+        ), "text/plain");
+
         $this->assertEquals(array("id" => 1), $parameters);
     }
-    
+
     public function testGetParametersParameterBeginning()
     {
         $request = new Request();
@@ -37,11 +37,11 @@ class RequestTests extends \PHPUnit_Framework_TestCase
             "controller" => "MyProject:MyController",
             "parameters" => array("id"),
             "parameterPositions" => array(1)
-        ), "");
-        
+        ), "text/plain");
+
         $this->assertEquals(array("id" => 1), $parameters);
     }
-    
+
     public function testGetParametersParameterMiddle()
     {
         $request = new Request();
@@ -49,11 +49,11 @@ class RequestTests extends \PHPUnit_Framework_TestCase
             "controller" => "MyProject:MyController",
             "parameters" => array("id"),
             "parameterPositions" => array(2)
-        ), "");
-        
+        ), "text/plain");
+
         $this->assertEquals(array("id" => 1), $parameters);
     }
-    
+
     public function testGetParametersParameterEnd()
     {
         $request = new Request();
@@ -61,8 +61,51 @@ class RequestTests extends \PHPUnit_Framework_TestCase
             "controller" => "MyProject:MyController",
             "parameters" => array("id"),
             "parameterPositions" => array(3)
-        ), "");
-        
+        ), "text/plain");
+
         $this->assertEquals(array("id" => 1), $parameters);
+    }
+
+    public function testGetParametersParameterData()
+    {
+        $request = new Request();
+
+        $parameters = $request->getParameters("/user/?parameter=1", array(
+            "controller" => "MyProject:MyController",
+            "parameters" => array(),
+            "parameterPositions" => array()
+        ), "text/plain");
+
+        $this->assertEquals(array("parameter" => "1"), $parameters);
+    }
+
+    public function testGetParametersPostData()
+    {
+        $request = $this->getMock("Grain\Request", array("getRawData"));
+        $request->method("getRawData")
+            ->will($this->returnValue("parameter=1"));
+
+        $parameters = $request->getParameters("/user", array(
+            "controller" => "MyProject:MyController",
+            "parameters" => array(),
+            "parameterPositions" => array()
+        ), "text/plain");
+
+        $this->assertEquals(array("parameter" => "1"), $parameters);
+    }
+
+    public function testGetParametersPostJsonData()
+    {
+        $request = $this->getMock("Grain\Request", array("getRawData"));
+        $request->method("getRawData")
+            ->will($this->returnValue("{\"parameter\":\"1\"}"));
+
+        $parameters = $request->getParameters("/user", array(
+            "controller" => "MyProject:MyController",
+            "parameters" => array(),
+            "parameterPositions" => array()
+        ), "application/json");
+
+        $this->assertEquals(array("parameter" => "1"), $parameters);
     }
 }
