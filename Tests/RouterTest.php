7cdc56927baca10dfc09cd0f3173b22221f0b06f
Kostas Milonas <milonas.ko@gmail.com>
2016-05-08 12:20:23 +0300

    moves routing related tests from core to router suite, adds missing router tests for url generation

--- Tests/RouterTest.php
+++ Tests/RouterTest.php
@@ -56,46 +56,275 @@ class RouterTest extends \PHPUnit_Framework_TestCase
         }
     }
 
-    public function testRouterRequestWithoutParameters()
+    public function testMatcherUrlWithoutParameters()
     {
         $matchedRoute = $this->router->matcher("/", "GET");
-        
+
         $this->assertEquals($this->findRouteByPath("/"), $matchedRoute);
     }
 
-    public function testRouterRequestOnlyOneParameter()
+    public function testMatcherUrlOnlyOneParameter()
     {
         $matchedRoute = $this->router->matcher("/1", "GET");
-        
+
         $this->assertEquals($this->findRouteByPath("/{id}"), $matchedRoute);
     }
-    
-    public function testRouterRequestBeginningParameter()
+
+    public function testMatcherUrlBeginningParameter()
     {
         $matchedRoute = $this->router->matcher("/1/edit", "GET");
-        
+
         $this->assertEquals($this->findRouteByPath("/{id}/edit"), $matchedRoute);
     }
-    
-    public function testRouterRequestMiddleParameter()
+
+    public function testMatcherUrlMiddleParameter()
     {
         $matchedRoute = $this->router->matcher("/user/1/edit", "GET");
-        
+
         $this->assertEquals($this->findRouteByPath("/user/{id}/edit"), $matchedRoute);
     }
-    
-    public function testRouterRequestEndParameter()
+
+    public function testMatcherUrlEndParameter()
     {
         $matchedRoute = $this->router->matcher("/user/edit/1", "GET");
-        
+
         $this->assertEquals($this->findRouteByPath("/user/edit/{id}"), $matchedRoute);
     }
 
-    public function testRouterNonExistentRoute()
+    public function testMatcherNonExistingRoute()
     {
         $matchedRoute = $this->router->matcher("/a/mess", "GET");
-        
+
         $this->assertEquals(null, $matchedRoute);
+    }
+
+    public function testGenerateUrlNoRoutesExist()
+    {
+        $router = new Router();
+
+        $url = $router->generateUrl("testRoute");
+
+        $this->assertNull($url);
+    }
+
+    public function testGenerateUrlNonExistingRoute()
+    {
+        $router = new Router();
+
+        $router->addRoute(array(
+            "path" => "/",
+            "method" => "GET",
+            "controller" => "MyProject:User:edit",
+            "routeName" => "testRoute"
+        ));
+
+        $url = $router->generateUrl("anotherTestRoute");
+
+        $this->assertNull($url);
+    }
+
+    public function testGenerateUrlExistingRouteWithoutParameters()
+    {
+        $router = new Router();
+
+        $router->addRoute(array(
+            "path" => "/",
+            "method" => "GET",
+            "controller" => "MyProject:User:edit",
+            "routeName" => "testRoute"
+        ));
+
+        $url = $router->generateUrl("testRoute");
+
+        $this->assertEquals("/", $url);
+    }
+
+    public function testGenerateUrlExistingRouteWithSingleParameter()
+    {
+        $router = new Router();
+
+        $router->addRoute(array(
+            "path" => "/{id}",
+            "method" => "GET",
+            "controller" => "MyProject:User:edit",
+            "routeName" => "testRoute"
+        ));
+
+        $url = $router->generateUrl("testRoute", array("id" => 1));
+
+        $this->assertEquals("/1", $url);
+    }
+
+    public function testGenerateUrlExistingRouteWithMultipleParameters()
+    {
+        $router = new Router();
+
+        $router->addRoute(array(
+            "path" => "/user/edit/{id}/{version}",
+            "method" => "GET",
+            "controller" => "MyProject:User:edit",
+            "routeName" => "testRoute"
+        ));
+
+        $url = $router->generateUrl("testRoute", array("id" => 1, "version" => 2));
+
+        $this->assertEquals("/user/edit/1/2", $url);
+    }
+
+    public function testMapRouteWithOutParameters()
+    {
+        $router = new Router();
+
+        $router->addRoute(array(
+            "path" => "/",
+            "method" => "GET",
+            "controller" => "MyProject:User:edit",
+            "routeName" => "testRoute"
+        ));
+
+        $this->assertEquals(
+            array(
+                array(
+                    "path" => "/",
+                    "method" => "GET",
+                    "controller" => "MyProject:User:edit",
+                    "parameters" => array(),
+                    "parameterPositions" => array(),
+                    "routeName" => "testRoute"
+                )
+            ),
+            $this->readAttribute($router, "routes")
+        );
+    }
+
+    public function testMapRouteWithParameterOneAndOnly()
+    {
+        $router = new Router();
+
+        $router->addRoute(array(
+            "path" => "/{id}",
+            "method" => "GET",
+            "controller" => "MyProject:User:edit",
+            "routeName" => "testRoute"
+        ));
+
+        $this->assertEquals(
+            array(
+                array(
+                    "path" => "/{id}",
+                    "method" => "GET",
+                    "controller" => "MyProject:User:edit",
+                    "parameters" => array("id"),
+                    "parameterPositions" => array(1),
+                    "routeName" => "testRoute"
+                )
+            ),
+            $this->readAttribute($router, "routes")
+        );
+    }
+
+    public function testMapRouteWithParameterBeginning()
+    {
+        $router = new Router();
+
+        $router->addRoute(array(
+            "path" => "/{id}/edit",
+            "method" => "GET",
+            "controller" => "MyProject:User:edit",
+            "routeName" => "testRoute"
+        ));
+
+        $this->assertEquals(
+            array(
+                array(
+                    "path" => "/{id}/edit",
+                    "method" => "GET",
+                    "controller" => "MyProject:User:edit",
+                    "parameters" => array("id"),
+                    "parameterPositions" => array(1),
+                    "routeName" => "testRoute"
+                )
+            ),
+            $this->readAttribute($router, "routes")
+        );
+    }
+
+    public function testMapRouteWithParameterMiddle()
+    {
+        $router = new Router();
+
+        $router->addRoute(array(
+            "path" => "/user/{id}/edit",
+            "method" => "GET",
+            "controller" => "MyProject:User:edit",
+            "routeName" => "testRoute"
+        ));
+
+        $this->assertEquals(
+            array(
+                array(
+                    "path" => "/user/{id}/edit",
+                    "method" => "GET",
+                    "controller" => "MyProject:User:edit",
+                    "parameters" => array("id"),
+                    "parameterPositions" => array(2),
+                    "routeName" => "testRoute"
+                )
+            ),
+            $this->readAttribute($router, "routes")
+        );
+    }
+
+    public function testMapRouteWithParameterEnd()
+    {
+        $router = new Router();
+
+        $router->addRoute(array(
+            "path" => "/user/edit/{id}",
+            "method" => "GET",
+            "controller" => "MyProject:User:edit",
+            "routeName" => "testRoute"
+        ));
+
+        $this->assertEquals(
+            array(
+                array(
+                    "path" => "/user/edit/{id}",
+                    "method" => "GET",
+                    "controller" => "MyProject:User:edit",
+                    "parameters" => array("id"),
+                    "parameterPositions" => array(3),
+                    "routeName" => "testRoute"
+                )
+            ),
+            $this->readAttribute($router, "routes")
+        );
+    }
+
+    public function testMapRouteWithTwoParameters()
+    {
+        $router = new Router();
+
+        $router->addRoute(array(
+            "path" => "/user/edit/{id}/{version}",
+            "method" => "GET",
+            "controller" => "MyProject:User:edit",
+            "routeName" => "testRoute"
+        ));
+
+        $this->assertEquals(
+            array(
+                array(
+                    "path" => "/user/edit/{id}/{version}",
+                    "method" => "GET",
+                    "controller" => "MyProject:User:edit",
+                    "parameters" => array("id", "version"),
+                    "parameterPositions" => array(3, 4),
+                    "routeName" => "testRoute"
+                )
+            ),
+            $this->readAttribute($router, "routes")
+        );
     }
 
     /**
